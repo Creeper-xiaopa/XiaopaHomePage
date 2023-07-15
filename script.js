36ffff1f8681c5a034c00b409d19c17b0bb89528
Creeper-xiaopa <99184151+Creeper-xiaopa@users.noreply.github.com>
2023-07-15 11:07:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openBilibiliSpace() {
+    window.open("https://space.bilibili.com/1668171984")
+}
+
+function openQQGroup() {
+    window.open("http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=h-Ng7EaAM5dXVSKTuUjtPtiqnnSgsGfO&authKey=Y4RbQjnV2CQy3XiS2%")
+
+}
+
+function OpenBlog() {
+    window.open("https://blog.xiaopa.eu.org")
+}
